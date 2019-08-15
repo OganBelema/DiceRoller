00b189f388c44041241c6e556c1d65ae3ac0c4de
tamunobelemamiebaka-ogan <belema@softcom.ng>
2019-08-15 23:24:54 +0100

    feat: display dice image when button clicked

--- app/src/main/java/com/oganbelema/diceroller/MainActivity.kt
+++ app/src/main/java/com/oganbelema/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.oganbelema.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +21,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
